9e56e4812520fd643d17549da2327ee5ee96e462
Muhammad Juan Akbar <juan.akbar@heavenhr.de>
2019-11-28 10:04:18 +0100

    add spring boot project

---
+++ src/main/java/it/iwkz/api/ApiIwkzApplication.java
@@ -0,0 +1,13 @@
+package it.iwkz.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIwkzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIwkzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/iwkz/api/ApiIwkzApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iwkz.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiIwkzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
